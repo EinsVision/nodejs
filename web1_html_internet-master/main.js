08a9fd75d8ba7e0f6fddda17ca8fb36b482e6f8a
jungchangdae <einsvision@gmail.com>
2021-02-04 23:24:31 +0900

    module 활용하기

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -2,40 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
-
-// object 만둘기
-var template = {
-  html: (title, list, body, control) => {
-    return `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-      </body>
-      </html>
-  
-    `;
-  },
-
-  list: (files) => {
-    var list = '<ul>';
-    var i = 0;
-    while(i< files.length ){
-      list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-      i++;
-  
-    }
-    list = list + '</ul>';
-    return list;
-  }
-}
+const template = require('../lib/template');
 
 const app = http.createServer(function(request,response){
     var _url = request.url;
