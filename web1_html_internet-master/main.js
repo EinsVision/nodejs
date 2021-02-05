daaff18e6cd147197162f09586a585690924bc8c
jungchangdae <einsvision@gmail.com>
2021-02-05 10:39:18 +0900

    sanitizeHtml moudule 추가 (express.js)

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,67 +1,59 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const template = require('../lib/template');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('../lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
-const app = http.createServer(function(request,response){
+var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
     if(pathname === '/'){
       if(queryData.id === undefined){
-
-        const testFolder = './data/';
-
-        fs.readdir(testFolder, (err, files) => { 
-            var title = 'Welcome';
-            var description = "Hello Node.js";  
-
-            var list = template.list(files);              
-            var html = template.html(title, list, `<h2>${title}</h2><p>${description}</p>`, 
-            `<a href="/create">create</a>`);
-
-            response.writeHead(200); // 파일을 성공적으로 전송했다.
-            response.end(html);
-
-          }) 
-      }
-      else{
-        const testFolder = './data/';
-
-        fs.readdir(testFolder, (err, files) => { 
-
-          fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-            var title = queryData.id;      
-            var list = template.list(files);
-            var html = template.html(title, list, `<h2>${title}</h2><p>${description}</p>`, 
-            `<a href="/create">create</a> 
-            <a href="/update?id=${title}">update</a>
-            <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <input type="submit" value="delete">
-            </form>
-            `);
-    
-            response.writeHead(200); // 파일을 성공적으로 전송했다.
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags:['h1']
+            });
+            var list = template.list(filelist);
+            var html = template.HTML(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.writeHead(200);
             response.end(html);
           });
         });
       }
     } else if(pathname === '/create'){
-      const testFolder = './data/';
-
-      fs.readdir(testFolder, (err, files) => { 
-          var title = 'WEB - create';
-          var list = template.list(files);
-   
-          var html = template.html(title, list, `
-          <form action="/create_process" method="POST">
-            <p>
-                <input type="text" name="title" placeholder="title">
-            </p>
-            
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" placeholder="description"></textarea>
             </p>
@@ -70,13 +62,10 @@ const app = http.createServer(function(request,response){
                 <input type="submit">
             </p>
           </form>
-          
-          `, '');
-
-          response.writeHead(200); // 파일을 성공적으로 전송했다.
-          response.end(html);
-
-        }) 
+        `, '');
+        response.writeHead(200);
+        response.end(html);
+      });
     } else if(pathname === '/create_process'){
       var body ='';
       request.on('data', function(data){
@@ -84,51 +73,40 @@ const app = http.createServer(function(request,response){
       });
 
       request.on('end', function(){
-        var post = qs.parse(body);
-        var title = post.title; // create로 부터 입력받은 title을 얻을 수 있다.
-        var descrtiption = post.description;
-        
-        fs.writeFile(`./data/${title}`, descrtiption, 'utf8', (err) => {
-          if (err) throw err;
-          
-          response.writeHead(302, {Location: `/?id=${title}`}); // 302는 사용자를 해당 웹페이지로 리다이렉션하는 명령어이다.
-          response.end();
-        });
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
       });
 
     } else if(pathname === '/update'){
-      
-      const testFolder = './data/';
-
-        fs.readdir(testFolder, (err, files) => { 
-
-          fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-            var title = queryData.id;      
-            var list = template.list(files);
-            var html = template.html(title, list, 
-              `
-              <form action="/update_process" method="POST">
-                <input type="hidden" name="id" value="${title}">
-                <p>
-                    <input type="text" name="title" placeholder="title" value="${title}">
-                </p>
-                
-                <p>
-                    <textarea name="description" placeholder="description">${description}</textarea>
-                </p>
-                
-                <p>
-                    <input type="submit">
-                </p>
-              </form>
-              `, 
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a> `);
-    
-            response.writeHead(200); // 파일을 성공적으로 전송했다.
-            response.end(html);
-          });
+      fs.readdir('./data', function(error, filelist){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
         });
-
+      });
     } else if(pathname === '/update_process'){
       var body ='';
       request.on('data', function(data){
@@ -136,42 +114,35 @@ const app = http.createServer(function(request,response){
       });
 
       request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        // console.log(post);
-        var title = post.title; // create로 부터 입력받은 title을 얻을 수 있다.
-        var descrtiption = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, (err) => {
-          fs.writeFile(`./data/${title}`, descrtiption, 'utf8', (err) => {
-          if (err) throw err;
-          
-          response.writeHead(302, {Location: `/?id=${title}`}); // 302는 사용자를 해당 웹페이지로 리다이렉션하는 명령어이다.
-          response.end();
-        });
-
-        })  
-      
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
       });
-
-    } else if(pathname === '/delete_process') {
-      var body ='';
+    } else if(pathname === '/delete_process'){
+      var body = '';
       request.on('data', function(data){
         body = body + data;
       });
 
       request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        fs.unlink(`./data/${id}`, (error)=>{
-          response.writeHead(302, {Location: `/`}); // 302는 사용자를 해당 웹페이지로 리다이렉션하는 명령어이다.
-          response.end();
-        })
-      
+          var post = qs.parse(body);
+          var id = post.id;
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
-    } else{
-      response.writeHead(404); // 파일을 찾을 수 없다.
-      response.end('Not Found');
-      return;
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
    });
 app.listen(3000); //3000번 포트를 바라보고 있다.
